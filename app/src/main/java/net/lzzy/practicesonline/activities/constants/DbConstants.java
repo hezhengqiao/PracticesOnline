3215a73bea8542522112fc2231aae766f3a459dd
hzq <1829007308@qq.com>
2019-04-18 18:03:09 +0800

    4月18号

---
+++ app/src/main/java/net/lzzy/practicesonline/activities/constants/DbConstants.java
@@ -0,0 +1,26 @@
+package net.lzzy.practicesonline.activities.constants;
+
+import net.lzzy.practicesonline.R;
+import net.lzzy.practicesonline.activities.utils.AppUtils;
+import net.lzzy.sqllib.DbPackager;
+
+/**
+ * Created by lzzy_gxy on 2019/3/11.
+ * Description:
+ */
+public final class DbConstants {
+    private DbConstants(){}
+    /**数据库名称*/
+    private static final String DB_NAME="practices.db";
+    /**版本号*/
+    private static final int DB_VERSION=1;
+/**打包数据库信息*/
+    public static DbPackager packager;
+
+    static {
+        packager=DbPackager.getInstance(AppUtils.getContext()
+                ,DB_NAME,DB_VERSION, R.raw.models);
+    }
+
+
+}
---
+++ app/src/main/java/net/lzzy/practicesonline/activities/models/FavoriteFactory.java
@@ -0,0 +1,78 @@
+package net.lzzy.practicesonline.activities.models;
+
+import android.app.Activity;
+
+import net.lzzy.practicesonline.activities.constants.DbConstants;
+import net.lzzy.practicesonline.activities.utils.AppUtils;
+import net.lzzy.sqllib.SqlRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author lzzy_gxy
+ * @date 2019/4/17
+ * Description:
+ */
+public class FavoriteFactory {
+    private static final FavoriteFactory OUR_INSTANCE = new FavoriteFactory();
+    private static  SqlRepository<Favorite> repository;
+
+    public static FavoriteFactory getInstance() {
+        return OUR_INSTANCE;
+    }
+
+    private FavoriteFactory() {
+        repository=new SqlRepository<>(AppUtils.getContext(),Favorite.class, DbConstants.packager);
+    }
+
+    private Favorite getFavoriteByQuestion(String questionId){
+        try {
+            List<Favorite> favorites=repository.getByKeyword(questionId,new String[]{Favorite.COL_QUESTION_ID},true);
+            if (favorites.size()>0){
+                return favorites.get(0);
+            }
+        } catch (IllegalAccessException|InstantiationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean isQuestionStarred(String questionId){
+        try {
+            List<Favorite> favorites=repository.getByKeyword(questionId,
+                    new String[]{Favorite.COL_QUESTION_ID},true);
+            return favorites.size()>0;
+        } catch (IllegalAccessException|InstantiationException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+/**收藏题目*/
+    public void starQuestion(UUID questionId){
+        Favorite favorite=getFavoriteByQuestion(questionId.toString());
+        if (favorite==null){
+            favorite=new Favorite();
+            favorite.setQuestionId(questionId);
+            repository.insert(favorite);
+        }
+    }
+/**取消收藏*/
+    public void cancelStarQuestion(UUID questionId){
+        Favorite favorite=getFavoriteByQuestion(questionId.toString());
+        if (favorite!=null){
+            repository.delete(favorite);
+        }
+    }
+
+    protected  String getDeleteString(String questionId){
+        Favorite favorite=getFavoriteByQuestion(questionId);
+        return favorite==null?null:repository.getDeleteString(favorite);
+    }
+
+    public List<Question> getAllFavorites(List<Question> questions){
+        return null;
+    }
+}
